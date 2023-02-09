1a932a0b905589a3101c52dca2937e74fa6ad6a3
Mauricio Carabajal <84579473+mauriciogc-sudo@users.noreply.github.com>
2023-02-09 18:52:57 -0300

    Update script_rundeck.sh

--- script_rundeck.sh
+++ script_rundeck.sh
@@ -4,9 +4,8 @@
 date=$(date +"%A %d %B")
 version=$(cat /etc/os-release | grep -i version)
 
+echo "this is script_rundeck.sh"
 echo "this is only a test"
 echo "time is: $date"
 echo -e "i'm running: \n"
 echo "$version"
-
-echo "this is script_rundeck.sh"

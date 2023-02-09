fcd0af2fa80b7eae99c699cfcc56bf34e8185b26
Mauricio Carabajal <84579473+mauriciogc-sudo@users.noreply.github.com>
2023-02-09 17:40:07 -0300

    Create script1_rundeck.sh

---
+++ reprepo/script1_rundeck.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+date=$(date +"%A %d %B")
+version=$(cat /etc/os-release | grep -i version)
+
+echo "this is only a test"
+echo "time is: $date"
+echo -e "i'm running: \n"
+echo "$version"
